f34db3dac0f63138ec0a178d535a3f633308d88e
djballowe <djballowe@gmail.com>
2024-09-15 09:29:09 -0700

    draw and info centered

--- client/main.go
+++ client/main.go
@@ -1,43 +1,43 @@
 package main
 
 import (
-	"fmt"
-	"tesla-app/client/api"
-	drawlogo "tesla-app/client/draw-status"
+	//	"fmt"
+	//	"tesla-app/client/api"
+	"tesla-app/client/draw-status"
 )
 
 func main() {
-	carDataResponse, error := api.CallGetVehicleData()
-	if error != nil {
-		fmt.Println(error.Error())
-		return
-	}
-
-	fmt.Println(carDataResponse.StatusCode)
-
-	if carDataResponse.StatusCode == 401 {
-		fmt.Println("This shouldnt fire")
-		authResponse, error := api.CallAuth()
-		if error != nil || authResponse.StatusCode != 200 {
-			fmt.Println(error.Error())
-			return
-		}
-
-		carDataResponse, error = api.CallGetVehicleData()
-		if error != nil {
-			fmt.Println(error.Error())
-			return
-		}
-	}
+	//	carDataResponse, error := api.CallGetVehicleData()
+	//	if error != nil {
+	//		fmt.Println(error.Error())
+	//		return
+	//	}
+	//
+	//	fmt.Println(carDataResponse.StatusCode)
+	//
+	//	if carDataResponse.StatusCode == 401 {
+	//		fmt.Println("This shouldnt fire")
+	//		authResponse, error := api.CallAuth()
+	//		if error != nil || authResponse.StatusCode != 200 {
+	//			fmt.Println(error.Error())
+	//			return
+	//		}
+	//
+	//		carDataResponse, error = api.CallGetVehicleData()
+	//		if error != nil {
+	//			fmt.Println(error.Error())
+	//			return
+	//		}
+	//	}
 
 	drawlogo.DrawStatus()
 
-	if carDataResponse.StatusCode != 200 {
-		fmt.Printf("Something went wrong: %s\n", carDataResponse.Body)
-		return
-	}
-
-	fmt.Println(carDataResponse.Body)
+	//	if carDataResponse.StatusCode != 200 {
+	//		fmt.Printf("Something went wrong: %s\n", carDataResponse.Body)
+	//		return
+	//	}
+	//
+	//	fmt.Println(carDataResponse.Body)
 
 	return
 }
