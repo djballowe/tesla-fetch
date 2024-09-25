b744d60aef966ca44f354cba4e95fba15d2f7664
David Ballowe <djballowe@gmail.com>
2024-09-24 21:31:10 -0700

    loading spinner

--- client/main.go
+++ client/main.go
@@ -2,43 +2,80 @@ package main
 
 import (
 	"fmt"
-	"tesla-app/client/api"
+	//	"tesla-app/client/api"
+	"sync"
 	"tesla-app/client/draw-status"
+	"time"
 )
 
 func main() {
-	carDataResponse, error := api.CallGetVehicleData()
-	if error != nil {
-		fmt.Println(error.Error())
-		return
-	}
+	//	carDataResponse, error := api.CallGetVehicleData()
+	//	if error != nil {
+	//		fmt.Println(error.Error())
+	//		return
+	//	}
+	//
+	//	fmt.Println(carDataResponse.StatusCode)
+	//
+	//	if carDataResponse.StatusCode == 401 {
+	//		authResponse, error := api.CallAuth()
+	//		if error != nil || authResponse.StatusCode != 200 {
+	//			fmt.Println(error.Error())
+	//			return
+	//		}
+	//
+	//		carDataResponse, error = api.CallGetVehicleData()
+	//		if error != nil {
+	//			fmt.Println(error.Error())
+	//			return
+	//		}
+	//	}
+	//
+	//	if carDataResponse.StatusCode != 200 {
+	//		if carDataResponse.StatusCode == 408 {
+	//			fmt.Printf("Error gathering vehicle data: Status Code %d vehicle is asleep\n", carDataResponse.StatusCode)
+	//			return
+	//		}
+	//		fmt.Printf("Error gathering vehicle data: Status Code %d\n", carDataResponse.StatusCode)
+	//		return
+	//	}
 
-	fmt.Println(carDataResponse.StatusCode)
+	var group sync.WaitGroup
+	group.Add(2)
 
-	if carDataResponse.StatusCode == 401 {
-		authResponse, error := api.CallAuth()
-		if error != nil || authResponse.StatusCode != 200 {
-			fmt.Println(error.Error())
-			return
-		}
+	flag := make(chan struct{})
 
-		carDataResponse, error = api.CallGetVehicleData()
-		if error != nil {
-			fmt.Println(error.Error())
-			return
-		}
-	}
+	go loadingSpinner(&group, flag)
+	go getVehicleData(&group, flag)
 
-	if carDataResponse.StatusCode != 200 {
-		if carDataResponse.StatusCode == 408 {
-			fmt.Printf("Error gathering vehicle data: Status Code %d vehicle is asleep\n", carDataResponse.StatusCode)
-			return
-		}
-		fmt.Printf("Error gathering vehicle data: Status Code %d\n", carDataResponse.StatusCode)
-		return
-	}
+	group.Wait()
 
-	drawlogo.DrawStatus(carDataResponse.Body)
+	drawlogo.DrawStatus()
 
 	return
+}
+
+func loadingSpinner(group *sync.WaitGroup, flag chan struct{}) {
+	defer group.Done()
+	loadSpinner := [10]string{"⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"}
+	idx := 0
+
+	for {
+		select {
+		case <-flag:
+			fmt.Printf("\r%s", "                         ")
+			return
+
+		default:
+			fmt.Printf("\r%s Fetching vehicle data", loadSpinner[idx%10])
+			time.Sleep(50 * time.Millisecond)
+			idx++
+		}
+	}
+}
+
+func getVehicleData(group *sync.WaitGroup, flag chan struct{}) {
+	defer group.Done()
+
+	close(flag)
 }
