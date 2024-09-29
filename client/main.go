98c563b58b0768a1850e6a49031fcb01c67358b7
David Ballowe <djballowe@gmail.com>
2024-09-29 08:43:08 -0700

    add loading status

--- client/main.go
+++ client/main.go
@@ -15,12 +15,15 @@ func main() {
 	group.Add(2)
 
 	done := make(chan struct{})
+	status := make(chan string)
 	vehicleDataChan := make(chan api.VehicleData)
 
-	go loadingSpinner(&group, done)
-	go getVehicleData(&group, done, vehicleDataChan)
+	go loading(&group, done, status)
+	go getVehicleData(&group, done, status, vehicleDataChan)
+
 	vehicleData := <-vehicleDataChan
 
+	close(status)
 	group.Wait()
 
 	drawlogo.DrawStatus(vehicleData)
@@ -28,10 +31,11 @@ func main() {
 	return
 }
 
-func loadingSpinner(group *sync.WaitGroup, done chan struct{}) {
+func loading(group *sync.WaitGroup, done chan struct{}, status chan string) {
 	defer group.Done()
 	loadSpinner := [10]string{"⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"}
 	idx := 0
+	msg := <-status
 
 	for {
 		select {
@@ -40,21 +44,23 @@ func loadingSpinner(group *sync.WaitGroup, done chan struct{}) {
 			return
 
 		default:
-			fmt.Printf("\r%s Fetching vehicle data", loadSpinner[idx%10])
+			fmt.Printf("\r%s %s", loadSpinner[idx%10], msg)
 			time.Sleep(50 * time.Millisecond)
 			idx++
 		}
 	}
 }
 
-func getVehicleData(group *sync.WaitGroup, done chan struct{}, vehicleDataChan chan api.VehicleData) error {
+func getVehicleData(group *sync.WaitGroup, done chan struct{}, status chan string, vehicleDataChan chan api.VehicleData) error {
 	defer group.Done()
 	carDataResponse, error := api.CallGetVehicleData()
+	status <- "Data"
 	if error != nil {
 		return error
 	}
 
 	if carDataResponse.StatusCode == 401 {
+		status <- "Auth"
 		authResponse, error := api.CallAuth()
 		if error != nil || authResponse.StatusCode != 200 {
 			return error
@@ -68,6 +74,7 @@ func getVehicleData(group *sync.WaitGroup, done chan struct{}, vehicleDataChan c
 
 	if carDataResponse.StatusCode != 200 {
 		if carDataResponse.StatusCode == 408 {
+			status <- "Asleep"
 			return errors.New(fmt.Sprintf("Error gathering vehicle data: Status Code %d vehicle is asleep", carDataResponse.StatusCode))
 		}
 		return errors.New(fmt.Sprintf("Error gathing vehicle data: Status Code %d", carDataResponse.StatusCode))
