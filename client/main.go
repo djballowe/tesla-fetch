b8c0155b412d312a00f6914a428ed5b56084f73a
djballowe <djballowe@gmail.com>
2024-09-29 20:52:12 -0700

    fix status

--- client/main.go
+++ client/main.go
@@ -9,33 +9,37 @@ import (
 	"time"
 )
 
-func main() {
+type result struct {
+	vehicleData api.VehicleData
+	err         error
+}
 
+func main() {
 	var group sync.WaitGroup
 	group.Add(2)
 
 	done := make(chan struct{})
-	status := make(chan string)
-	vehicleDataChan := make(chan api.VehicleData)
+	dataChan := make(chan result)
 
-	go loading(&group, done, status)
-	go getVehicleData(&group, done, status, vehicleDataChan)
+	go loading(&group, done)
+	go getVehicleData(&group, done, dataChan)
 
-	vehicleData := <-vehicleDataChan
-
-	close(status)
 	group.Wait()
 
-	drawlogo.DrawStatus(vehicleData)
+	res := <-dataChan
+	if res.err != nil {
+		fmt.Println(res.err.Error())
+	}
+
+	drawlogo.DrawStatus(res.vehicleData)
 
 	return
 }
 
-func loading(group *sync.WaitGroup, done chan struct{}, status chan string) {
+func loading(group *sync.WaitGroup, done chan struct{}) {
 	defer group.Done()
 	loadSpinner := [10]string{"⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"}
 	idx := 0
-	msg := <-status
 
 	for {
 		select {
@@ -44,43 +48,45 @@ func loading(group *sync.WaitGroup, done chan struct{}, status chan string) {
 			return
 
 		default:
-			fmt.Printf("\r%s %s", loadSpinner[idx%10], msg)
+			fmt.Printf("\r%s Fetching vehicle data", loadSpinner[idx%10])
 			time.Sleep(50 * time.Millisecond)
 			idx++
 		}
 	}
 }
 
-func getVehicleData(group *sync.WaitGroup, done chan struct{}, status chan string, vehicleDataChan chan api.VehicleData) error {
+func getVehicleData(group *sync.WaitGroup, done chan struct{}, dataChan chan result) {
 	defer group.Done()
+	defer close(done)
 	carDataResponse, error := api.CallGetVehicleData()
-	status <- "Data"
 	if error != nil {
-		return error
+		dataChan <- result{err: error}
+		return
 	}
 
 	if carDataResponse.StatusCode == 401 {
-		status <- "Auth"
 		authResponse, error := api.CallAuth()
 		if error != nil || authResponse.StatusCode != 200 {
-			return error
+			dataChan <- result{err: error}
+			return
 		}
 
 		carDataResponse, error = api.CallGetVehicleData()
 		if error != nil {
-			return error
+			dataChan <- result{err: error}
+			return
 		}
 	}
 
 	if carDataResponse.StatusCode != 200 {
 		if carDataResponse.StatusCode == 408 {
-			status <- "Asleep"
-			return errors.New(fmt.Sprintf("Error gathering vehicle data: Status Code %d vehicle is asleep", carDataResponse.StatusCode))
+			dataChan <- result{err: errors.New(fmt.Sprintf("Error gathering vehicle data: status code %d vehicle is asleep", carDataResponse.StatusCode))}
+			return
 		}
-		return errors.New(fmt.Sprintf("Error gathing vehicle data: Status Code %d", carDataResponse.StatusCode))
+		dataChan <- result{err: errors.New(fmt.Sprintf("Error gathing vehicle data: Status Code %d", carDataResponse.StatusCode))}
+		return
 	}
 
-	vehicleDataChan <- carDataResponse.Body
-	close(done)
-	return nil
+	dataChan <- result{vehicleData: carDataResponse.Body, err: nil}
+	return
 }
