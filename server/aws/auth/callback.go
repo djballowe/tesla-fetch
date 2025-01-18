30ae3eb16566a6501848ace464c27c059d4857d1
djballowe <djballowe@gmail.com>
2025-01-17 21:42:35 -0800

    aws auth and get secrets function

--- server/aws/auth/callback.go
+++ server/aws/auth/callback.go
@@ -2,20 +2,13 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"encoding/json"
 	"log"
-	"net/url"
+	awshelpers "tesla-app/server/aws/secrets"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
 )
-
-type Token struct {
-	AccessToken  string `json:"access_token"`
-	RefreshToken string `json:"refresh_token"`
-}
 
 func callbackHandler(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	log.Printf("QueryStringParameters: %+v", event.QueryStringParameters)
@@ -24,7 +17,6 @@ func callbackHandler(ctx context.Context, event events.APIGatewayProxyRequest) (
 	state := event.QueryStringParameters["state"]
 	prevState := event.QueryStringParameters["prevState"]
 
-	//	authStatus := false
 	if code == "" || state == "" {
 		log.Println("Missing request data")
 		return events.APIGatewayProxyResponse{
@@ -41,58 +33,31 @@ func callbackHandler(ctx context.Context, event events.APIGatewayProxyRequest) (
 		}, nil
 	}
 
-	_, err := exchangeCodeForToken(code)
-	if err != nil {
+	tokens, err := awshelpers.ExchangeCodeForToken(code)
+	if err != nil || tokens.AccessToken == "" || tokens.RefreshToken == "" {
 		log.Println("Could not gather token: ", err)
 		return events.APIGatewayProxyResponse{
 			StatusCode: 500,
 			Body:       `{"message: could not gather token data"}`,
 		}, nil
 	}
-//	log.Println("token: ", tokens.AccessToken)
+
+	response, err := json.Marshal(tokens)
+	if err != nil {
+		log.Println("Could not marshal response body: ", err)
+		return events.APIGatewayProxyResponse{
+			StatusCode: 500,
+			Body:       `{"message: could not gather token data"}`,
+		}, nil
+	}
 
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       `{"message: success"}`,
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+		Body: string(response),
 	}, nil
-}
-
-func exchangeCodeForToken(code string) (*Token, error) {
-	log.Println("Exchanging code for token...")
-
-	baseUrl, err := url.Parse("https://auth.tesla.com/oauth2/v3/token")
-	if err != nil {
-		return nil, err
-	}
-	tokenUrl := baseUrl.String()
-
-	secrets, err := getSecrets("tesla_creds")
-	if err != nil {
-		return nil, err
-	}
-
-	log.Println("Token url: ", tokenUrl)
-	log.Println("clientId: ", secrets)
-
-	return nil, nil
-}
-
-func getSecrets(secret string) (string, error) {
-	cfg, err := config.LoadDefaultConfig(context.TODO())
-	if err != nil {
-		return "", fmt.Errorf("failed to load AWS config: %w", err)
-	}
-
-	client := secretsmanager.NewFromConfig(cfg)
-
-	output, err := client.GetSecretValue(context.TODO(), &secretsmanager.GetSecretValueInput{
-		SecretId: &secret,
-	})
-	if err != nil {
-		return "", fmt.Errorf("failed to retrieve secret: %w", err)
-	}
-
-	return *output.SecretString, nil
 }
 
 func main() {
