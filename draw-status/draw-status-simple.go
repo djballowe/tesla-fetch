e630fdfb3d3aeebbfe0ed3a5ca7c4f72f0da0bc4
David Ballowe <101905433+djballowe@users.noreply.github.com>
2026-01-10 09:39:14 -0800

    print strings not json for simple print (#46)

--- draw-status/draw-status-simple.go
+++ draw-status/draw-status-simple.go
@@ -1,17 +1,40 @@
 package drawstatus
 
 import (
-	"encoding/json"
 	"fmt"
+	"strconv"
 	"tfetch/data"
 )
 
 func (d *DrawService) DrawStatusSimple(vehicleData *data.VehicleData) error {
-	jsonData, err := json.MarshalIndent(vehicleData, "", " ")
-	if err != nil {
-		return err
+	carMap := map[string]string{
+		"models": "Model S",
+		"model3": "Model 3",
+		"modelx": "Model X",
+		"modely": "Model Y",
 	}
 
-	fmt.Println(string(jsonData))
+	info := []string{
+		"Nickname: " + vehicleData.VehicleName,
+		"Model: " + carMap[vehicleData.CarType],
+		"Color: " + vehicleData.Color,
+		"Miles: " + strconv.Itoa(vehicleData.Odometer),
+		"Charge: " + strconv.Itoa(vehicleData.BatteryLevel) + "%",
+		"Charge Limit: " + strconv.Itoa(vehicleData.ChargeLimitSoc),
+		"Range: " + strconv.FormatFloat(vehicleData.BatteryRange, 'f', 1, 64) + " Miles",
+		"Charge State: " + vehicleData.ChargingState,
+		"Charge Rate: " + strconv.FormatFloat(vehicleData.ChargeRate, 'f', 1, 64),
+		"Climate On: " + strconv.FormatBool(vehicleData.IsClimateOn),
+		"Climate Inside: " + strconv.Itoa(convertClimate(vehicleData.InsideTemp)) + "°F",
+		"Climate Outside: " + strconv.Itoa(convertClimate(vehicleData.OutsideTemp)) + "°F",
+		"Driver Temp Setting: " + strconv.Itoa(convertClimate(int(vehicleData.DriverTempSetting))) + "°F",
+		"Passenger Temp Setting: " + strconv.Itoa(convertClimate(int(vehicleData.PassengerTempSetting))) + "°F",
+		"Locked: " + strconv.FormatBool(vehicleData.Locked),
+	}
+
+	for _, data := range info {
+		fmt.Println(data)
+	}
+
 	return nil
 }
